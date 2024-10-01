18fb034b3976b7d439da28d7457706c6198e428f
Andre-Raymond <andre-raymond.socard@eleve.ensai.fr>
2024-10-01 12:03:01 +0200

    debut classe et pour base de données

--- src/business_object/Equipe.py
+++ src/business_object/Equipe.py
@@ -1,2 +1,49 @@
 class Equipe:
-    
+     """
+    Classe représentant un Joueur
+
+    Attributs
+    ----------
+    id_joueur : int
+        identifiant
+    pseudo : str
+        pseudo du joueur
+    mdp : str
+        le mot de passe du joueur
+    age : int
+        age du joueur
+    mail : str
+        mail du joueur
+    fan_pokemon : bool
+        indique si le joueur est un fan de Pokemon
+    """
+
+    def __init__(self, match_id, equipe_nom, equipe_image, equipe_score, equipe_winner,
+                 shots, goals, saves, assists, score, shooting_percentage, date, ligue,
+                  region, stage,):
+
+        self.match_id = match_id                     # ID du match
+        self.equipe_nom = equipe_nom                 # Nom de l'équipe
+        self.equipe_image = equipe_image             # Image de l'équipe
+        self.equipe_score = equipe_score             # Score de l'équipe
+        self.equipe_winner = equipe_winner           # Vainqueur (booléen ou indicatif)
+        self.shots = shots                           # Nombre de tirs
+        self.goals = goals                           # Nombre de buts
+        self.saves = saves                           # Sauvegardes
+        self.assists = assists                       # Assistances
+        self.score = score                           # Score total
+        self.shooting_percentage = shooting_percentage  # Pourcentage de tir
+        self.date = date                             # Date du match
+        self.ligue = ligue                           # Ligue du match
+        self.region = region                         # Région du match
+        self.stage = stage                           # Étape du tournoi ou du match
+
+    # Méthode pour afficher les informations du match
+   def __str__(self):
+        """Permet d'afficher les informations de l'equipe"""
+        return f"Joueur({self.pseudo}, {self.age} ans)"
+
+    def afficher_statistiques(self) -> list[str]:
+        """Retourne les attributs de l'equipe"""
+        return [self.pseudo, self.age, self.mail, self.fan_pokemon]
+        # pas fait
--- src/dao/equipe_dao.py
+++ src/dao/equipe_dao.py
@@ -8,7 +8,7 @@ from dao.db_connection import DBConnection
 from business_object.joueur import Joueur
 
 
-class JoueurDao(metaclass=Singleton):
+class EquipeDao(metaclass=Singleton):
     """Classe contenant les méthodes pour accéder aux Joueurs de la base de données"""
 
     @log
@@ -17,7 +17,7 @@ class JoueurDao(metaclass=Singleton):
 
         Parameters
         ----------
-        joueur : Joueur
+        Equipe : Equipe
 
         Returns
         -------
@@ -27,71 +27,49 @@ class JoueurDao(metaclass=Singleton):
         """
 
         res = None
-
         try:
+            # Connexion à la base de données
             with DBConnection().connection as connection:
                 with connection.cursor() as cursor:
+                    # Requête d'insertion SQL avec RETURNING pour récupérer l'ID
                     cursor.execute(
-                        "INSERT INTO joueur(pseudo, mdp, age, mail, fan_pokemon) VALUES        "
-                        "(%(pseudo)s, %(mdp)s, %(age)s, %(mail)s, %(fan_pokemon)s)             "
-                        "  RETURNING id_joueur;                                                ",
+                        """
+                        INSERT INTO Equipe (match_id, equipe_nom, equipe_image, equipe_score, equipe_winner,
+                                        shots, goals, saves, assists, score, shooting_percentage, date, ligue, region, stage)
+                        VALUES (%(match_id)s, %(equipe_nom)s, %(equipe_image)s, %(equipe_score)s, %(equipe_winner)s,
+                                %(shots)s, %(goals)s, %(saves)s, %(assists)s, %(score)s, %(shooting_percentage)s, %(date)s,
+                                %(ligue)s, %(region)s, %(stage)s)
+                        RETURNING match_id;
+                        """,
                         {
-                            "pseudo": joueur.pseudo,
-                            "mdp": joueur.mdp,
-                            "age": joueur.age,
-                            "mail": joueur.mail,
-                            "fan_pokemon": joueur.fan_pokemon,
+                            "match_id": equipe.match_id,
+                            "equipe_nom": equipe.equipe_nom,
+                            "equipe_image": equipe.equipe_image,
+                            "equipe_score": equipe.equipe_score,
+                            "equipe_winner": equipe.equipe_winner,
+                            "shots": equipe.shots,
+                            "goals": equipe.goals,
+                            "saves": equipe.saves,
+                            "assists": equipe.assists,
+                            "score": equipe.score,
+                            "shooting_percentage": equipe.shooting_percentage,
+                            "date": equipe.date,
+                            "ligue": equipe.ligue,
+                            "region": equipe.region,
+                            "stage": equipe.stage,
                         },
                     )
+                    # Récupérer l'ID généré
                     res = cursor.fetchone()
         except Exception as e:
             logging.info(e)
 
         created = False
         if res:
-            joueur.id_joueur = res["id_joueur"]
+            equipe.match_id = res["match_id"]  # Si un ID est retourné, on le met à jour
             created = True
 
         return created
-
-def trouver_par_id(self, id_joueur) -> Joueur:
-        """trouver un joueur grace à son id
-
-        Parameters
-        ----------
-        id_joueur : int
-            numéro id du joueur que l'on souhaite trouver
-
-        Returns
-        -------
-        joueur : Joueur
-            renvoie le joueur que l'on cherche par id
-        """
-        try:
-            with DBConnection().connection as connection:
-                with connection.cursor() as cursor:
-                    cursor.execute(
-                        "SELECT *                           "
-                        "  FROM joueur                      "
-                        " WHERE id_joueur = %(id_joueur)s;  ",
-                        {"id_joueur": id_joueur},
-                    )
-                    res = cursor.fetchone()
-        except Exception as e:
-            logging.info(e)
-            raise
-
-        joueur = None
-        if res:
-            joueur = Joueur(
-                pseudo=res["pseudo"],
-                age=res["age"],
-                mail=res["mail"],
-                fan_pokemon=res["fan_pokemon"],
-                id_joueur=res["id_joueur"],
-            )
-
-        return joueur
 
     @log
     def lister_tous(self) -> list[Joueur]:
@@ -137,119 +115,3 @@ def trouver_par_id(self, id_joueur) -> Joueur:
         return liste_joueurs
 
     @log
-    def modifier(self, joueur) -> bool:
-        """Modification d'un joueur dans la base de données
-
-        Parameters
-        ----------
-        joueur : Joueur
-
-        Returns
-        -------
-        created : bool
-            True si la modification est un succès
-            False sinon
-        """
-
-        res = None
-
-        try:
-            with DBConnection().connection as connection:
-                with connection.cursor() as cursor:
-                    cursor.execute(
-                        "UPDATE joueur                                      "
-                        "   SET pseudo      = %(pseudo)s,                   "
-                        "       mdp         = %(mdp)s,                      "
-                        "       age         = %(age)s,                      "
-                        "       mail        = %(mail)s,                     "
-                        "       fan_pokemon = %(fan_pokemon)s               "
-                        " WHERE id_joueur = %(id_joueur)s;                  ",
-                        {
-                            "pseudo": joueur.pseudo,
-                            "mdp": joueur.mdp,
-                            "age": joueur.age,
-                            "mail": joueur.mail,
-                            "fan_pokemon": joueur.fan_pokemon,
-                            "id_joueur": joueur.id_joueur,
-                        },
-                    )
-                    res = cursor.rowcount
-        except Exception as e:
-            logging.info(e)
-
-        return res == 1
-
-    @log
-    def supprimer(self, joueur) -> bool:
-        """Suppression d'un joueur dans la base de données
-
-        Parameters
-        ----------
-        joueur : Joueur
-            joueur à supprimer de la base de données
-
-        Returns
-        -------
-            True si le joueur a bien été supprimé
-        """
-
-        try:
-            with DBConnection().connection as connection:
-                with connection.cursor() as cursor:
-                    # Supprimer le compte d'un joueur
-                    cursor.execute(
-                        "DELETE FROM joueur                  "
-                        " WHERE id_joueur=%(id_joueur)s      ",
-                        {"id_joueur": joueur.id_joueur},
-                    )
-                    res = cursor.rowcount
-        except Exception as e:
-            logging.info(e)
-            raise
-
-        return res > 0
-
-    @log
-    def se_connecter(self, pseudo, mdp) -> Joueur:
-        """se connecter grâce à son pseudo et son mot de passe
-
-        Parameters
-        ----------
-        pseudo : str
-            pseudo du joueur que l'on souhaite trouver
-        mdp : str
-            mot de passe du joueur
-
-        Returns
-        -------
-        joueur : Joueur
-            renvoie le joueur que l'on cherche
-        """
-        res = None
-        try:
-            with DBConnection().connection as connection:
-                with connection.cursor() as cursor:
-                    cursor.execute(
-                        "SELECT *                           "
-                        "  FROM joueur                      "
-                        " WHERE pseudo = %(pseudo)s         "
-                        "   AND mdp = %(mdp)s;              ",
-                        {"pseudo": pseudo, "mdp": mdp},
-                    )
-                    res = cursor.fetchone()
-        except Exception as e:
-            logging.info(e)
-
-        joueur = None
-
-        if res:
-            joueur = Joueur(
-                pseudo=res["pseudo"],
-                mdp=res["mdp"],
-                age=res["age"],
-                mail=res["mail"],
-                fan_pokemon=res["fan_pokemon"],
-                id_joueur=res["id_joueur"],
-            )
-
-        return joueur
