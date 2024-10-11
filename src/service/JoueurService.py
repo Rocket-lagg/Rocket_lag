35b123e77fcc3be4e403ab0ad0c6b19b459508bb
Paulin Lemoine <paulin.lemoine@eleve.ensai.fr>
2024-10-11 12:01:40 +0200

    creation structure de match equipe et joueur service

---
+++ src/service/JoueurService.py
@@ -0,0 +1,7 @@
+from src.utils.singleton import Singleton
+
+
+class JoueurService(metaclass=Singleton):
+
+    def afficher_statistiques(nom: str):
+        pass
---
+++ src/service/MatchService.py
@@ -0,0 +1,7 @@
+from src.utils.singleton import Singleton
+
+
+class MatchService(metaclass=Singleton):
+
+    def afficher_statistiques(nom: str):
+        pass
