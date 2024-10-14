6ec25455f66c6f042dc324ea9352d0315c483a1e
Andre-Raymond <andre-raymond.socard@eleve.ensai.fr>
2024-10-14 17:18:40 +0200

    oui

--- data/Crea_table_utile.sql
+++ data/Crea_table_utile.sql
@@ -1,25 +1,31 @@
-CREATE TABLE matches (
+CREATE TABLE Equipe (
     match_id VARCHAR(255),
     equipe_nom VARCHAR(255),
-    equipe_image VARCHAR(255),
-    equipe_score INT,
-    equipe_winner BOOLEAN,
+    joueur_nom VARCHAR(255),
+    region VARCHAR(255),
+    equipe_winner INT,
     shots INT,
     goals INT,
     saves INT,
     assists INT,
     score INT,
     shooting_percentage FLOAT,
-    date TIMESTAMP,
+    demo_infligées INT,
+    demo_reçues INT,
+    goal_participation FLOAT,
+    rating FLOAT,
+    time_defensive_third FLOAT,
+    time_neutral_third FLOAT,
+    time_offensive_third FLOAT,
     ligue VARCHAR(255),
-    region VARCHAR(50),
-    stage VARCHAR(50)
+    stage VARCHAR(255)
 );
 
-CREATE TABLE players (
+CREATE TABLE Joueur (
     match_id VARCHAR(255),
     equipe_nom VARCHAR(255),
     joueur_nom VARCHAR(255),
+    region VARCHAR(255),
     nationalite VARCHAR(50),
     shots INT,
     goals INT,
@@ -34,4 +40,8 @@ CREATE TABLE players (
     time_defensive_third FLOAT,
     time_neutral_third FLOAT,
     time_offensive_third FLOAT
+);
+
+CREATE TABLE matches (
+
 );
