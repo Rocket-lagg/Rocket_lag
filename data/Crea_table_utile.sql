9f85d7df78130041501b639bb4d8a4e9c51e8baf
Andre-Raymond <andre-raymond.socard@eleve.ensai.fr>
2024-10-17 01:45:25 +0200

    pour initialiser les tables

--- data/Crea_table_utile.sql
+++ data/Crea_table_utile.sql
@@ -1,8 +1,6 @@
 CREATE TABLE Equipe (
     match_id VARCHAR(255),
     equipe_nom VARCHAR(255),
-    joueur_nom VARCHAR(255),
-    region VARCHAR(255),
     equipe_winner INT,
     shots INT,
     goals INT,
@@ -13,19 +11,17 @@ CREATE TABLE Equipe (
     demo_infligées INT,
     demo_reçues INT,
     goal_participation FLOAT,
-    rating FLOAT,
     time_defensive_third FLOAT,
     time_neutral_third FLOAT,
     time_offensive_third FLOAT,
-    ligue VARCHAR(255),
-    stage VARCHAR(255)
+    PRIMARY KEY (match_id, equipe_nom),  -- Clé primaire composée
+    FOREIGN KEY (match_id) REFERENCES matches(match_id)
 );
 
 CREATE TABLE Joueur (
     match_id VARCHAR(255),
     equipe_nom VARCHAR(255),
     joueur_nom VARCHAR(255),
-    region VARCHAR(255),
     nationalite VARCHAR(50),
     shots INT,
     goals INT,
@@ -39,9 +35,14 @@ CREATE TABLE Joueur (
     rating FLOAT,
     time_defensive_third FLOAT,
     time_neutral_third FLOAT,
-    time_offensive_third FLOAT
+    time_offensive_third FLOAT,
+    PRIMARY KEY (match_id, joueur_nom),  -- Clé primaire composée
+    FOREIGN KEY (match_id) REFERENCES matches(match_id)  -- Clé étrangère qui fait référence à 'ligue_id' dans la table 'leagues'
 );
 
 CREATE TABLE matches (
-
+    match_id VARCHAR(255) PRIMARY KEY,
+    date DATE,
+    ligue VARCHAR(255),
+    region VARCHAR(255)
 );
