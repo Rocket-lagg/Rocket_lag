218750fa2f97f1892e8071898873fa5da78a9392
Paulin Lemoine <paulin.lemoine@eleve.ensai.fr>
2024-10-21 16:35:06 +0200

    début de code

--- src/service/consulter_stat.py
+++ src/service/consulter_stat.py
@@ -1,11 +1,10 @@
 from src.utils.singleton import Singleton
 from src.dao.equipe_dao import EquipeDao
+from src.dao.joueur_dao import JoueurDao
 
 
-class EquipeService(metaclass=Singleton):
+class ConsulterStats(metaclass=Singleton):
 
-    def afficher_statistiques(nom: str):
-        pass
-
-    def liste_joueurs(nom: str):
-        pass
+    def stats_joueurs(self, nom_joueur):
+        if not isinstance(nom_joueur, str):
+            raise TypeError("nom_joueur doit être une instance de str")
