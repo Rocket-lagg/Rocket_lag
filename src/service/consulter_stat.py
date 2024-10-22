c12701833570e39689f2f62e67ede54ceb80beb5
Paulin Lemoine <paulin.lemoine@eleve.ensai.fr>
2024-10-22 09:20:39 +0200

    début stats

--- src/service/consulter_stat.py
+++ src/service/consulter_stat.py
@@ -1,10 +1,16 @@
 from src.utils.singleton import Singleton
 from src.dao.equipe_dao import EquipeDao
 from src.dao.joueur_dao import JoueurDao
+from src.business_object.joueur import Joueur
 
 
 class ConsulterStats(metaclass=Singleton):
+    """Une classe service qui affiche les statistiques par joueur, équipe et match"""
 
     def stats_joueurs(self, nom_joueur):
+        """Une fonction qui permet d'afficher les statistiques par joueur"""
         if not isinstance(nom_joueur, str):
             raise TypeError("nom_joueur doit être une instance de str")
+        joueur = JoueurDao(nom_joueur)
+        if not joueur:
+            raise ValueError("'nom_joueur' ne correspond à aucun joueur de la base de données")
