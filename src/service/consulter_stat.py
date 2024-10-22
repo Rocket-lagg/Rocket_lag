fc01f480f3312a4f5174c9db8b4eb379bfa9752b
Paulin Lemoine <paulin.lemoine@eleve.ensai.fr>
2024-10-22 09:50:33 +0200

    partie facile de stats_joueurs

--- src/service/consulter_stat.py
+++ src/service/consulter_stat.py
@@ -14,3 +14,23 @@ class ConsulterStats(metaclass=Singleton):
         joueur = JoueurDao(nom_joueur)
         if not joueur:
             raise ValueError("'nom_joueur' ne correspond à aucun joueur de la base de données")
+        goals = joueur.goals
+        # résultat TODO
+        assists = joueur.assists
+        shots = joueur.shots
+        saves = joueur.saves
+        # rating TODO
+        shooting_percentage = joueur.shooting_percentage
+        demolitions = joueur.demo_inflige
+        # indice offensif/défensif TODO
+        # indice de performance TODO
+        # indice de pression TODO
+        print(
+            f"Statistiques pour le joueur {nom_joueur} depuis le début de la saison :\n"
+            f"Buts : {goals}\n"
+            f"Passes décisives : {assists}\n"
+            f"Tirs : {shots}\n"
+            f"Arrêts : {saves}\n"
+            f"Pourcentage de tirs cadrés : {shooting_percentage}\n"
+            f"Démolitions : {demolitions}"
+        )
